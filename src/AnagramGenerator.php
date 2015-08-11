342a8cb9ef640f2ad14e0cb058247210de98d43b
Kenny Grage & Jeff Franco <student@epicodus.com>
2015-08-11 15:37:42 -0700

    finish three tests

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -1,36 +1,26 @@
 <?php
     class AnagramGenerator {
-        function makeAnagram($input) {
-            $chars = str_split($input);
-            sort($chars);
-            return implode("", $chars);
 
+        function makeAnagram($input, $anagram) {
+            $input_array = str_split($input);
+            sort($input_array);
+            $matched_words = array();
+            $un_matched_words = array();
+
+
+            foreach ($anagram as $ana) {
+                $ana_array = str_split($ana);
+                sort($ana_array);
+                if ($input_array == $ana_array) {
+                    array_push($matched_words, $ana);
+                }
+                else {
+                    array_push($un_matched_words, $ana);
+                }
+
+
+            }
+            return array($matched_words, $un_matched_words);
         }
     }
 ?>
-
-
-
-
-<!-- class AnagramGenerator {
-    function makeAnagram($chars, $size, $combinations = array()) {
-        //$chars = str_split($input);
-
-        if (empty($combinations)) {
-            $combinations = $chars;
-        }
-
-
-        if ($size == 1) {
-            return $combinations;
-        }
-        $new_combinations = array();
-
-        foreach ($combinations as $combination) {
-            foreach ($chars as $char) {
-                $new_combinations[] = $combination . $char;
-            }
-        }
-        return sampling($chars, $size - 1, $new_combinations);
-    }
-} -->
--- tests/AnaagramGeneratorTest.php
+++ tests/AnaagramGeneratorTest.php
@@ -6,13 +6,43 @@
             //Arrange
             $test_AnagramGenerator = new AnagramGenerator;
             $input = "on";
+            $input2 = array("no");
 
             //Act
-            $result = $test_AnagramGenerator->makeAnagram($input);
+            $result = $test_AnagramGenerator->makeAnagram($input, $input2);
 
             //Assert
-            $this->assertEquals("no", $result);
+            $this->assertEquals(array(array("no"), array()), $result);
 
         }
+
+        function test_makeAnagram_threeLetters() {
+            //Arrange
+            $test_AnagramGenerator = new AnagramGenerator;
+            $input = "cab";
+            $input2 = array("abc");
+
+            //Act
+            $result = $test_AnagramGenerator->makeAnagram($input, $input2);
+
+            //Assert
+            $this->assertEquals(array(array("abc"), array()), $result);
+        }
+
+        function test_makeAnagram_twoWordsAgainstMainWord() {
+            //Arrange
+            $test_AnagramGenerator = new AnagramGenerator;
+            $input = "bread";
+            $input2 = array("beard", "wind");
+
+
+            //Act
+            $result = $test_AnagramGenerator->makeAnagram($input, $input2);
+
+            //Assert
+            $this->assertEquals(array(array("beard"), array("wind")), $result);
+        }
+
+
     }
 ?>
